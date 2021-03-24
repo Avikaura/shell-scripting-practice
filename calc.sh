f8259e92a3d4f0874d00cf8433ed5d3788a75aad
Avi Kaura <avikaura20@gmail.com>
2021-03-24 00:52:24 -0400

    user capture for Exit function been recorded, successfully

--- calc.sh
+++ calc.sh
@@ -14,6 +14,9 @@ echo "Greetings User! Welcome to Avi Kaura's Calculator"
 # Then, capture the user selection
 echo " please select a operation from the above list"
 read user_input
-# If the selection matches a supported operation, execute the operation.
+# If the selection matches a supported operation, execute the operation
+if [[ user_input == Exit || user_input == E || user_input == e ]];then
+	exit
+fi
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
